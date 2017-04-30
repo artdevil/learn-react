9413a7eee09e20553534a3b9ee4c3cd8719e6d13
adhiguna utama sabril <adhiguna.sabril@gmail.com>
2017-04-30 14:01:15 +0700

    update components

--- App.jsx
+++ App.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import Header from './components/header.jsx';
+import Content from './components/content.jsx';
+import TableRow from './components/table_row.jsx';
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      data:
+      [
+        {
+          "id": 1,
+          "name": "Foo",
+          "age": "20"
+        },
+        {
+          "id": 2,
+          "name": "Bar",
+          "age": "30"
+        },
+        {
+          "id": 3,
+          "name": "Baz",
+          "age": "40"
+        },
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
-        Hello world!!!
+        <Header />
+
+        <table>
+          <tbody>
+            {this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
+          </tbody>
+        </table>
+
+        <Content />
       </div>
     )
   }
