3a2b7ab9e7aa3574683a3f5feaadf8b2d1db09ca
adhiguna utama sabril <adhiguna.sabril@gmail.com>
2017-05-01 10:41:15 +0700

    correction

--- App.jsx
+++ App.jsx
@@ -1,46 +1,15 @@
 import React from 'react';
 import Header from './components/header.jsx';
 import Content from './components/content.jsx';
-import TableRow from './components/table_row.jsx';
+import Footer from './components/footer.jsx';
 
 export default class App extends React.Component {
-  constructor(){
-    super();
-
-    this.state = {
-      data:
-      [
-        {
-          "id": 1,
-          "name": "Foo",
-          "age": "20"
-        },
-        {
-          "id": 2,
-          "name": "Bar",
-          "age": "30"
-        },
-        {
-          "id": 3,
-          "name": "Baz",
-          "age": "40"
-        },
-      ]
-    }
-  }
-
   render() {
     return (
       <div>
         <Header />
-
-        <table>
-          <tbody>
-            {this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
-          </tbody>
-        </table>
-
         <Content />
+        <Footer />
       </div>
     )
   }
--- components/content.jsx
+++ components/content.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import TableRow from './table_row.jsx';
 
 export default class Content extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {
+      data:
+      [
+        {
+          "id": 1,
+          "name": "Foo",
+          "age": "20"
+        },
+        {
+          "id": 2,
+          "name": "Bar",
+          "age": "30"
+        },
+        {
+          "id": 3,
+          "name": "Baz",
+          "age": "40"
+        }
+      ]
+    }
+  }
+
   render(){
     return (
-      <div>
-        <h2>content</h2>
-        <p>The content text!</p>
-      </div>
+      <table>
+        <tbody>
+          {this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
+        </tbody>
+      </table>
     )
   }
 }
