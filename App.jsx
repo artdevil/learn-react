fcb5ba3162aadc4b2ca0b4715d94155571440157
adhiguna utama sabril <adhiguna.sabril@gmail.com>
2017-05-02 18:37:24 +0700

    update life cycle

--- App.jsx
+++ App.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import Content from './components/content.jsx'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello world!!!
-      </div>
-    )
-  }
+
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         data: 0
+      }
+
+      this.setNewNumber = this.setNewNumber.bind(this)
+   };
+
+   setNewNumber() {
+      this.setState({data: this.state.data + 1})
+   }
+
+   render() {
+      return (
+         <div>
+            <button onClick={this.setNewNumber}>INCREMENT</button>
+            <Content myNumber={this.state.data}></Content>
+         </div>
+      );
+   }
 }
